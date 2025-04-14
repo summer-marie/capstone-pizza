2a7e273bef8797dfd7920a4ba921a0520f16f632
Summer Halsey <shalsey@perseverenow.org>
2025-04-14 13:27:12 -0700

    Adds update one to backend

--- server/builders/builderUpdateOne.js
+++ server/builders/builderUpdateOne.js
@@ -1,1 +1,25 @@
 // TODO: needed to update pizza from admin menu
+
+import builderModel from "./builderModel"
+
+const pizzaUpdateOne = async (req, res) => {
+  const { id } = req.params
+
+  const { pizzaName, base, sauce, toppings, price, image } = req.body
+
+  const pizza = await builderModel.findOneAndUpdate(
+    { _id: id },
+    {
+      pizzaName,
+      base,
+      sauce,
+      toppings,
+      price,
+      image,
+    }
+  )
+  console.log("Pizza updated YAYYY!!", pizza)
+  res.status(200).json({ success: true, pizza: pizza })
+}
+
+export default pizzaUpdateOne
