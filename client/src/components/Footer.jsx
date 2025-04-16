2fbcb249d77918122653fe80125c7b1fcc220188
Summer Halsey <shalsey@perseverenow.org>
2025-04-16 14:10:05 -0700

    Adds sign up page for new admins. Updated Admin login form.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <>
       <footer
         className='bg-white 
-      rounded-lg shadow-sm fixed bottom-0 w-full mt-5'
+      rounded-lg shadow-sm fixed bottom-0 w-full'
       >
         <div className='w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between h-auto'>
           <span className='text-sm text-gray-500 sm:text-center'>
--- client/src/customer/Contact.jsx
+++ client/src/customer/Contact.jsx
@@ -2,35 +2,23 @@ const Contact = () => {
   return (
     <div>
       Contact page
-      <form id="imgUploader" className='max-w-lg mx-auto'>
+      <form id='imgUploader' className='max-w-lg mx-auto'>
         <label
-          className='block mb-2 text-sm font-medium text-gray-900'
+          className='block mb-2 text-sm font-medium pl-2 text-gray-900'
           htmlFor='user_avatar'
         >
           Upload file
         </label>
         <input
-          className='block w-full text-sm focus:outline-none text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 '
+          className='block w-full text-lg focus:outline-none p-2 text-gray-800 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 '
           aria-describedby='user_avatar_help'
           id='user_avatar'
           type='file'
         />
-        <div
-          className='mt-1 text-sm text-gray-500'
-          id='user_avatar_help'
-        >
-          A profile picture is useful to confirm your are logged into your
-          account
+        <div className='mt-1 text-sm text-gray-500' id='user_avatar_help'>
+          Add picture off desired pizza
         </div>
       </form>
-
-
-
-
-
-
-
-      
     </div>
   )
 }
