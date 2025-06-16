5d7f88ac986250244e1df348f75d1d5a67e9b520
summer-marie <summer.halsey0318@gmail.com>
2025-06-16 12:58:15 -0700

    Add toJSON transformation to message model: standardize JSON format and ID handling across models

--- server/messages/msgModel.js
+++ server/messages/msgModel.js
@@ -1,4 +1,15 @@
 import mongoose from "mongoose";
 import messageSchema from "./msgSchema.js";
 
-export default mongoose.model("Message", messageSchema);
+messageSchema.set("toJSON", {
+  transform: (doc, ret, options) => {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  },
+});
+
+const messageModel = mongoose.model("Message", messageSchema);
+
+export default messageModel;
